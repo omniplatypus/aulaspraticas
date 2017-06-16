beffd6062e5588766ad899d96dcffa5017fa859e
gabriel <gabriel@OmniPC>
2017-06-16 02:20:18 -0300

    Commit no1

--- POO - Aula Prática - Camadas e MVC/src/br/ufrpe/social_network/negocio/beans/Post.java
+++ POO - Aula Prática - Camadas e MVC/src/br/ufrpe/social_network/negocio/beans/Post.java
@@ -4,14 +4,17 @@ import java.util.ArrayList;
 
 public class Post {
     
+	private static long nextId=1;
+	
     private long id;
     private String texto;
     private Person author;
     private ArrayList<Post> comments;
 
     public Post(String texto, Person author) {
-        // TODO Implementar a mesma estratégia da classe Person para geração de ID  
-        
+        // Implementar a mesma estratégia da classe Person para geração de ID  
+        this.id = nextId;
+        nextId += 1;
         this.texto = texto;
         this.author = author;
         this.comments = new ArrayList<>();
@@ -26,9 +29,34 @@ public class Post {
     }
     
     
-    // TODO gerar todos os métodos get/set, exceto o setId
     
-    public void addComment(Post comment) {
+    //  gerar todos os métodos get/set, exceto o setId
+    
+    public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public Person getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(Person author) {
+		this.author = author;
+	}
+
+	public ArrayList<Post> getComments() {
+		return comments;
+	}
+
+	public void setComments(ArrayList<Post> comments) {
+		this.comments = comments;
+	}
+
+	public void addComment(Post comment) {
         if (comment != null) {
             this.comments.add(comment);
         }
@@ -42,7 +70,11 @@ public class Post {
     
     @Override
     public String toString() {
-        // TODO Implementar método toString da classe Person
-        return "Coloque sua implementação aqui";
+        //  Implementar método toString da classe Person
+    	String str = this.getAuthor().toString() + "\n" + this.getTexto();
+        return str;
     }
+    
 }
+
+

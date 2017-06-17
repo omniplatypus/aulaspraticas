6bef469c36fd6c2acf0262d683c96285fb15693d
Gabriel Ferreira Melo <gmelo.cc@gmail.com>
2017-06-16 21:42:59 -0300

    PostDAO pronto

--- POO - Aula Prática - Camadas e MVC/src/br/ufrpe/social_network/dao/PostDAO.java
+++ POO - Aula Prática - Camadas e MVC/src/br/ufrpe/social_network/dao/PostDAO.java
@@ -1,12 +1,11 @@
 package br.ufrpe.social_network.dao;
 
 import java.util.ArrayList;
-import br.ufrpe.social_network.negocio.beans.Person;
 import br.ufrpe.social_network.negocio.beans.Post;
 
 public class PostDAO {
     
-	private ArrayList<Post> Posts = new ArrayList();
+	private static ArrayList<Post> posts = new ArrayList<>();
     // implementar singleton para esta classe - PostDAO
     private static PostDAO instance;
 	private PostDAO() {
@@ -18,8 +17,43 @@ public class PostDAO {
     	}
     	return instance;
     }
-    //TODO Implementar CRUD para classe básica Post usando ArrayList
+    // Implementar CRUD para classe básica Post usando ArrayList
+   
     public void createPost(Post post){
+    	posts.add(post);
+    }
+    
+    public boolean deletePost(long id){
+    	boolean removed = false;
+    	Post found = this.findPost(id);
+    	if(found.getId() == id){
+    		posts.remove(found);
+    		removed = true;
+    	}
+    	return removed;
+    }
+    
+    public Post findPost(long id){
+    	Post result = null;;
+    	for (Post post : posts) {
+			if(post.getId() == id){
+				result = post;
+			}
+		}
+    	return result;
+    }
+    
+    
+    public boolean updatePost(Post post){
+    	boolean found = false;
+    	for(int i=0; i<posts.size() && !found; i++){
+    		if(post.getId() == posts.get(i).getId()){
+    			posts.remove(i);
+    			posts.set(i, post);
+    			found = true;
+    		}
+    	}
+    	return found;
     	
     }
     /*

940f7c7723bc510580d0c401a256a5f491e7c57b
Gabriel Ferreira Melo <gmelo.cc@gmail.com>
2017-06-16 21:58:18 -0300

    PersonDAO pronto

--- POO - Aula Prática - Camadas e MVC/src/br/ufrpe/social_network/dao/PersonDAO.java
+++ POO - Aula Prática - Camadas e MVC/src/br/ufrpe/social_network/dao/PersonDAO.java
@@ -1,9 +1,14 @@
 package br.ufrpe.social_network.dao;
 
+import java.util.ArrayList;
+import br.ufrpe.social_network.negocio.beans.Person;
+
 public class PersonDAO {
     // implementar singleton para esta classe - PersonDAO
 
 	public static PersonDAO instance;
+	
+	private static ArrayList<Person> persons = new ArrayList<>();
 	
 	private PersonDAO(){	
 	}
@@ -15,8 +20,44 @@ public class PersonDAO {
 		return instance;
 	}
     
-    //TODO Implementar CRUD para classe básica Person usando array
+    // Implementar CRUD para classe básica Person usando array
     
+	public void createPerson(Person person){
+    	persons.add(person);
+    }
+    
+    public boolean deletePerson(long id){
+    	boolean removed = false;
+    	Person found = this.findPerson(id);
+    	if(found.getId() == id){
+    		persons.remove(found);
+    		removed = true;
+    	}
+    	return removed;
+    }
+    
+    public Person findPerson(long id){
+    	Person result = null;;
+    	for (Person person : persons) {
+			if(person.getId() == id){
+				result = person;
+			}
+		}
+    	return result;
+    }
+    
+    
+    public boolean updatePerson(Person person){
+    	boolean found = false;
+    	for(int i=0; i<persons.size() && !found; i++){
+    		if(person.getId() == persons.get(i).getId()){
+    			persons.remove(i);
+    			persons.set(i, person);
+    			found = true;
+    		}
+    	}
+    	return found;
+    }
     /*
      * Antes de implementar os métodos, pense bem na assinatura dos mesmos
      */
